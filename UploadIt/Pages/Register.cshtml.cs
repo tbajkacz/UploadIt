f805ab245e29e9b8336945bfdc2bf89651bea966
menidas98 <menidas98@gmail.com>
2019-04-21 12:11:08 +0200

    #5.1 remote work share

--- UploadIt/Pages/Register.cshtml.cs
+++ UploadIt/Pages/Register.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,15 @@ namespace UploadIt.Pages
 {
     public class RegisterModel : PageModel
     {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
         public void OnGet()
         {
         }
---
+++ UploadIt/wwwroot/scripts/Register.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../lib/jquery.min.js" />
+import * as constants from "./Constants.js";
+
+$("#register_form").on("submit", (ev) => {
+    ev.preventDefault();
+    let form = new FormData();
+    let data = Object.values(ev.currentTarget).slice(0, 3);
+    form.append("email", data[0].value);
+    form.append("username", data[1].value);
+    form.append("password", data[2].value);
+
+    $.ajax({
+        url: constants.apiUrl + "/Account/Register",
+        method: "post",
+        data: form,
+        success: (resp) => {
+            console.log(resp);
+        }
+
+    });
+})
