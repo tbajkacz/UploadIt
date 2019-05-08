02f7ec0042ea37aff72318c936cba7207e079672
menidas98 <menidas98@gmail.com>
2019-05-08 10:42:50 +0200

    #11 Changed modal display position Added missing js maps Added a progress indicator for ajax requests

---
+++ UploadIt/wwwroot/scripts/AjaxProgressIndicator.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../lib/jquery.min.js" />
+
+
+
+$(document).on("ajaxStart", () => {
+    $("#loadingModal").modal("show");
+});
+
+$(document).on("ajaxSuccess", () => {
+    $("#loadingModal").modal("hide");
+})
